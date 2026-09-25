9294233ad697210f5e05b5f90a09ea6eeae80f3b
mayurimdeore <149568591+mayurimdeore@users.noreply.github.com>
2026-09-25 15:38:54 +0530

    Create SubscriptionRepository interface for JPA

---
+++ src/main/java/com/subscription/billing/repository/SubscriptionRepository.java
@@ -0,0 +1,10 @@
+package com.subscription.billing.repository;
+
+import com.subscription.billing.model.Subscription;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
+
+}
