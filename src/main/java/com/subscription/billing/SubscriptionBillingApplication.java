55466a53bab253411f2958212454f29da7bb5a21
mayurimdeore <149568591+mayurimdeore@users.noreply.github.com>
2026-09-25 15:22:13 +0530

    Create SubscriptionBillingApplication class

---
+++ src/main/java/com/subscription/billing/SubscriptionBillingApplication.java
@@ -0,0 +1,12 @@
+package com.subscription.billing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionBillingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubscriptionBillingApplication.class, args);
+    }
+}
